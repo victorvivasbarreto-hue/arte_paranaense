4fba43c91e14f08e30597e0f5451b59ba504a8b0
victorvivasbarreto-hue <victor.vivas.barreto@escola.pr.gov.br>
2026-09-15 11:15:17 -0300

    eu atualizei o codigo

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const botoesCurtir = document.querySelectorAll(".curtir");
+botoesCurtir.forEach(function(botoesCurtir) {
+    let curtiu = false;
+    botaoCurtir.addEventListener("click", curtir);
+    function curtir(){
+        const contador = botaoCurtir.querySelector("span");
+        if(curtiu === false){
+            contador.textContent++;
+            curtiu = true;}
+            else{
+                con.textContent--;
+                curtiu = false;
+            }
+        }
+    });
 
